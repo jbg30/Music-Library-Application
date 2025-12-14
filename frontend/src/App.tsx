20ff7e2fbda960d2e6e889cc33e8d8c069091693
jbg30 <jaredgonzales2577@gmail.com>
2025-12-13 19:34:37 -0800

    Admin route/controls for creating/deleting a song

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import { Button } from "./components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
-      <Button variant={"outline"}>This is a button</Button>
+      <header>
+      <SignedOut>
+        <SignInButton>
+        <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
