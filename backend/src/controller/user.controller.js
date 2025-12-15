14bf9ad973e3edc49d2c8cea03ba1510037cb182
jbg30 <jaredgonzales2577@gmail.com>
2025-12-15 04:31:18 -0800

    Update user.controller.js

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -3,7 +3,7 @@ import { User } from "../models/user.model.js";
 export const getAllUsers = async (req, res, next) => {
     try {
         const currentUserId = req.auth.userId;
-        const users = await User.find();
+        const users = await User.find({clerkId: {$ne: currentUserId}});
         res.status(200).json(users);
     } catch (error) {
         next(error);
