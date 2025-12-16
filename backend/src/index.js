fe03957d82a38b1b9cfa134ba266064781b9d327
jbg30 <jaredgonzales2577@gmail.com>
2025-12-16 01:49:45 -0800

    1

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,8 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
-import cron from "node-cron";
 import fs from "fs";
+import cron from "node-cron";
 
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
@@ -22,24 +22,26 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    }));
-
-app.use(express.json()); // to parse req.body
-app.use(clerkMiddleware()); // this will add auth to req obj => req.user 
-app.use(
-    fileUpload({
-        useTempFiles: true,
-        tempFileDir: path.join(__dirname, "tmp"), 
-        createParentPath: true,
-        limits: {
-        fileSize: 10 * 1024 * 1024, // 10MB
-        },
-    })
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
 );
 
+app.use(express.json()); // to parse req.body
+app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: path.join(__dirname, "tmp"),
+		createParentPath: true,
+		limits: {
+			fileSize: 10 * 1024 * 1024, // 10MB  max file size
+		},
+	})
+);
+
+// cron jobs
 const tempDir = path.join(process.cwd(), "tmp");
 cron.schedule("0 * * * *", () => {
 	if (fs.existsSync(tempDir)) {
@@ -55,28 +57,26 @@ cron.schedule("0 * * * *", () => {
 	}
 });
 
-
 app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("/*", (req, res) => {
+	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 	});
 }
 
 // error handler
 app.use((err, req, res, next) => {
-    res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
 });
 
-
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-    connectDB();
+	console.log("Server is running on port " + PORT);
+	connectDB();
 });
