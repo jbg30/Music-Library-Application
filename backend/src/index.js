52ec4670f49fdd895d779a1f174e359b68c49beb
jbg30 <jaredgonzales2577@gmail.com>
2025-12-16 02:44:10 -0800

    4

--- backend/src/index.js
+++ backend/src/index.js
@@ -65,10 +65,13 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("/(.*)", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(frontendPath));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 // error handler
